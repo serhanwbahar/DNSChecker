501129f68ef41f66119e320e91b291c593e926f2
swb <serhan@swb.sh>
2023-03-16 23:19:24 +0000

    release v1

--- README.md
+++ README.md
@@ -1,23 +1,61 @@
-# DNSChecker
+# DNS Record Checker Script
 
-A bash script that check all DNS records for a domain or subdomain.
+This bash script checks DNS records for a given domain and its subdomains (optional). It supports checking A, AAAA, CNAME, MX, NS, SOA, TXT, and ANY records. The script queries multiple DNS servers to ensure accurate results.
+
+# Requirements
+
+* Bash (4.x or higher)
+* `dig` (part of the dnsutils package)
 
 # Installation
 
-Just Download the .sh file on your computer. (Tested on MacOS (M1 and Intel), Ubuntu, Kali and Parrot OS)
+1. Download the script or clone the repository.
+2. Make the script executable: chmod +x dnsChecker.sh
 
-# How to run
+# Usage
 
-# Step 1:
+```
+./dnsChecker.sh <domain_name> [subdomain] [record_type]
+```
 
-Open terminal where you download the file and run
+`<domain_name>`: The domain name for which you want to check the DNS records (e.g., example.com).
+`[subdomain]` (optional): The subdomain to check, if required (e.g., www).
+`[record_type]` (optional): The type of DNS record you want to check. Supported record types are A, AAAA, CNAME, MX, NS, SOA, TXT, and ANY. The default is ANY, which checks all supported record types.
 
-chmod +x DNSCheckerV1.sh
+# Examples
 
-# Step 2:
+1. Check all supported DNS records for example.com:
 
-Execute the script
+```
+./dnsChecker.sh example.com
+```
 
-./DNSCheckerV1.sh domainname.com
+2. Check A records for example.com:
 
-Just that.
+```
+./dnsChecker.sh example.com "" A
+```
+
+3. Check all supported DNS records for the www subdomain of example.com:
+
+```
+./dnsChecker.sh example.com www
+```
+
+4. heck MX records for the mail subdomain of example.com:
+
+```
+./dnsChecker.sh example.com mail MX
+```
+
+# Error Codes
+
+`101`: Missing domain name
+`102`: Invalid record type
+`103`: Unable to reach DNS server
+
+These error codes are displayed alongside descriptive error messages to guide the user when encountering issues.
+
+# License
+
+This project is licensed under the [MIT License](https://opensource.org/license/mit/).
--- dnsChecker.sh
+++ dnsChecker.sh
@@ -1,49 +1,63 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Error: Missing domain name!"
+    echo "Usage: $0 <domain_name> [subdomain] [record_type]"
+    exit 101
+fi
+
 domain_name=$1
-subdomain=$2
-record_type=$3
+subdomain=${2:-""}
+record_type=${3:-"ANY"}
 
 if [ ! -z "$subdomain" ]; then
     domain_name="${subdomain}.${domain_name}"
 fi
 
-if [ -z "$record_type" ]; then
-    record_type="ANY"
+supported_record_types=("A" "AAAA" "CNAME" "MX" "NS" "SOA" "TXT" "ANY")
+
+if [[ ! " ${supported_record_types[@]} " =~ " ${record_type} " ]]; then
+    echo "Error: Invalid record type!"
+    echo "Supported record types: ${supported_record_types[*]}"
+    exit 102
 fi
 
 dns_servers=(
-    "1.1.1.1"         # Cloudflare
-    "208.67.222.222"  # OpenDNS
-    "8.8.8.8"         # Google DNS
-    "8.26.56.26"      # Comodo Secure DNS
-    "9.9.9.9"         # Quad9
+    "1.1.1.1"       # Cloudflare
+    "208.67.222.222" # OpenDNS
+    "8.8.8.8"        # Google DNS
+    "8.26.56.26"     # Comodo Secure DNS
+    "9.9.9.9"        # Quad9
 )
 
-function query_record() {
+function check_records() {
     local domain=$1
-    local dns_server=$2
-    local record=$3
+    local server=$2
+    local rtype=${3:-"ANY"}
 
-    echo "$(dig +nocmd "${domain}" @${dns_server} ${record} +noall +answer | awk '{print substr($0, index($0,$4))}')"
-}
-
-function check_record() {
-    local domain=$1
-    local record=$2
-
-    for dns_server in "${dns_servers[@]}"; do
-        result=$(query_record "${domain}" "${dns_server}" "${record}")
-
-        if [ "${result}" != "out; no servers could be reached" ]; then
-            echo "${result}" | sed "s/${record}/\n&/g" | sed '/^$/d'
-            return
+    if [ "$rtype" == "ANY" ]; then
+        for rt in "${supported_record_types[@]}"; do
+            if [ "$rt" != "ANY" ]; then
+                check_records "$domain" "$server" "$rt"
+            fi
+        done
+    else
+        records=$(dig +nocmd "$domain" @"$server" "$rtype" +noall +answer | awk '{print substr($0, index($0,$4))}')
+        if [ "$records" != "out; no servers could be reached" ]; then
+            if [ -n "$records" ]; then
+                echo "$records" | sed "s/$rtype/\n&/g" | sed '/^$/d'
+            else
+                echo "No $rtype records found."
+            fi
+        else
+            echo "Error: Unable to reach DNS server $server!"
+            exit 103
         fi
-    done
-
-    echo "Error!"
-    exit 110
+    fi
 }
 
-echo "${record_type^^} RECORDS"
-check_record "${domain_name}" "${record_type}"
+for dns_server in "${dns_servers[@]}"; do
+    echo "Checking $record_type records using DNS server $dns_server:"
+    check_records "$domain_name" "$dns_server" "$record_type"
+    echo
+done
